144036c7c22fb8f9447bad144110b7c3a948fbd8
LucasMagaldi <lucas.magaldi@hotmail.com>
2023-01-03 01:47:15 -0300

    writting tests to Comment form component

---
+++ src/tests/CommetForm.test.js
@@ -0,0 +1,17 @@
+import { screen , render, fireEvent } from "@testing-library/react";
+import CommentForm from "../components/CommentForm";
+
+test('Initializing component', () => {
+
+});
+
+test("Enable submit button on type and checkbox list", () => {
+    render(<CommentForm/>);
+    const checkbox = screen.getByLabelText("I agree to terms ans conditions", {exact: false});
+    const submitButton = screen.getByRole("button", {name:'comment', exact: false});
+    fireEvent.click(checkbox);
+
+    expect(submitButton).toBeEnabled();
+    fireEvent.click(checkbox);
+    expect(submitButton).toBeDisabled();
+})
