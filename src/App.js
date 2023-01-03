55531fd85e181259016c939de49b2a5d07cb9b30
LucasMagaldi <lucas.magaldi@hotmail.com>
2023-01-02 23:33:34 -0300

    stating

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <button>testing</button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('testing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonlement = screen.getByRole("button", {name: "testing", exact: false});
+  expect(buttonlement).toBeInTheDocument();
 });
