913b639761feafe62fa51047d745cdb974e72056
LucasMagaldi <lucas.magaldi@hotmail.com>
2023-01-05 01:19:20 -0300

    Comment form

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function CommentForm() {
+function CommentForm({setComments}) {
+
+  const [text, setText] = useState('');
+  const [checked, setCheck] = useState(false);
+
   return (
-    <div>CommentForm</div>
+    <div>
+      <h2>Component form</h2>
+      <input 
+        placeholder='Write your comments'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <input
+        type="checkbox"
+        id="checkbox"
+        defaultChecked={checked}
+        onChange={() => setCheck(!checked)}
+      />
+
+    <label htmlFor='checkbox'>
+        I agree to the terms
+    </label>
+    <button disabled={!checked || !text}>
+      submit comment
+    </button>
+
+    </div>
+    
   )
 }
 
